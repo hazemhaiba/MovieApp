5b0ac17debdba60b81fe94404cc27a9aa87b1500
hazemhaiba <hazemhaiba@gmail.com>
2016-02-01 15:21:05 +0200

    Update Important_Strings.java

--- app/src/main/java/com/garagy/movieapp/Important_Strings.java
+++ app/src/main/java/com/garagy/movieapp/Important_Strings.java
@@ -4,7 +4,7 @@ package com.garagy.movieapp;
  * Created by The Maestro on 12/21/2015.
  */
 public class Important_Strings {
-    public static final String App_key = "6f9f2b35e31c8079b7c296023b82cef9";
+    public static final String App_key = "Add_Your_API_KEY_HERE";
     public static final String Base_url = "http://api.themoviedb.org/3/";
     public static final String Movie_popular = "movie/popular";
     public static final String Movie_High_rated = "movie/top_rated";
